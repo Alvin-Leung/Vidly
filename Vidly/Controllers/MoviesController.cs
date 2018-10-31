133f276e15181227031a30ffa12f2eaacda30442
alvin <alvin.leung92@gmail.com>
2018-10-31 00:07:20 -0700

    Update bootstrap from version 3.3.7 to 4.1.3. Implement basic Customer and Movie pages.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+            
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>()
+            {
+                new Movie() {Name = "Die Hard II"},
+                new Movie() {Name = "Don't Think Twice"}
+            };
+        }
+    }
+}
