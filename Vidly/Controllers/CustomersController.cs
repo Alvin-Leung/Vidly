ddea36b167bf29b3ebd997f1cf5c9a8dffb52856
alvin <alvin.leung92@gmail.com>
2018-11-03 12:19:32 -0700

    Add create and edit routes for Customer

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -22,21 +23,62 @@ namespace Vidly.Controllers
             this._context.Dispose();
         }
 
-        public ViewResult Index()
+        public ActionResult Index()
         {
             var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
         }
 
-        public ActionResult Details(int id)
+        public ActionResult New()
         {
-            var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
+            var membershipTypes = this._context.MembershipTypes.ToList();
+
+            var viewModel = new CustomerFormViewModel()
+            {
+                MembershipTypes = membershipTypes
+            };
+
+            return View("CustomerForm", viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = this._context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
+            {
                 return HttpNotFound();
+            }
 
-            return View(customer);
+            var viewModel = new CustomerFormViewModel()
+            {
+                Customer = customer,
+                MembershipTypes = this._context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if (customer.Id == 0)
+            {
+                this._context.Customers.Add(customer);
+            }
+            else
+            {
+                var customerInDb = this._context.Customers.Single(c => c.Id == customer.Id);
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthdate = customer.Birthdate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            }
+
+            this._context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
         }
     }
 }
